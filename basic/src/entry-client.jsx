978e938d1e18a1e1f3c6c39753758764baa1ae4d
Muzaffar Ali <92566448+muzaffar16@users.noreply.github.com>
2025-07-29 10:58:11 +0500

    Update entry-client.jsx

--- basic/src/entry-client.jsx
+++ basic/src/entry-client.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-// ✅ Make sure these variables are not undefined
-// console.log('👀 window.__SSR_DATA__:', window.__SSR_DATA__)
-// console.log('👀 window.__INITIAL_CONFIG__:', window.__INITIAL_CONFIG__)
 
 const ssrData = window.__SSR_DATA__
 const initialConfig = window.__INITIAL_CONFIG__
